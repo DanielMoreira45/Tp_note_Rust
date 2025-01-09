3673db56f3eda769f7c2443a8b2a909ff0191525
Daniel Moreira <daniel.moreira@etu.univ-orleans.fr>
2025-01-09 12:21:47 +0100

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,41 @@ fn seuillage(img: &mut RgbImage){
     println!("Luminosité est {}", pixel_luminositer(img, 0, 0));
 }
 
-fn main() -> Result<(), ImageError> {
-    let img_file = ImageReader::open("images/images.jpeg")?;
-    let mut img: image::RgbImage = img_file.decode()?.into_rgb8();
-    println!("Image dimensions: {}x{}", img.width(), img.height());
-    println!("Pixel au coordonnées (32, 32): {:?}", img.get_pixel(32, 32));
+fn save_img(img: &RgbImage, path: &str) -> Result<(), ImageError> {
+    img.save(path)?;
+    return Ok(())
+}
 
+fn image_quadrillage(img: &mut RgbImage) -> Result<(), ImageError> {
     for (x,y,color) in img.enumerate_pixels_mut() {
-        // if (x+y) % 2 == 0 {
-        //     *color = Rgb([255, 255, 255]);
-        // }
+        if (x+y) % 2 == 0 {
+            *color = Rgb([255, 255, 255]);
+        }
+    }
+    save_img(img, "src/images/output_quadrillage.png")?;
+    return Ok(())
+}
+
+fn image_noir_blanc(img: &mut RgbImage)-> Result<(), ImageError> {
+    for (_x,_y,color) in img.enumerate_pixels_mut() {
         if color[0] > 128 {
             *color = Rgb([255, 255, 255]);
         } else {
             *color = Rgb([0, 0, 0]);
         }
     }
-    img.save("images/output.png")?;
+    save_img(img, "src/images/output_noir_blanc.png")?;
+    return Ok(())
+}
+
+fn main() -> Result<(), ImageError> {
+    let img_file = ImageReader::open("src/images/images.jpeg")?;
+    let mut img: image::RgbImage = img_file.decode()?.into_rgb8();
+    println!("Image dimensions: {}x{}", img.width(), img.height());
+    println!("Pixel au coordonnées (32, 32): {:?}", img.get_pixel(32, 32));
+
+    image_quadrillage(&mut img)?;
+    image_noir_blanc(&mut img)?;
 
     return Ok(())
 }
